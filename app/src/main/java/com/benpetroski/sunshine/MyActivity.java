4e0f70867d301f07b890bf4a8c47db3209b108bf
Ben Petroski <ben@benpetroski.com>
2014-07-15 15:33:21 -0700

    Minor changes to add ArrayAdapter and an ArrayList of forecast strings.

--- app/src/main/java/com/benpetroski/sunshine/MyActivity.java
+++ app/src/main/java/com/benpetroski/sunshine/MyActivity.java
@@ -1,7 +1,6 @@
 package com.benpetroski.sunshine;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,8 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MyActivity extends Activity {
@@ -58,6 +60,19 @@ public class MyActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+
+            String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/46",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Rainy - 64/51",
+                "Fri - Foggy - 70/46",
+                "Sat - Sunny - 76/68"
+            };
+
+            List<String> weekForecast = new ArrayList(Arrays.asList(forecastArray));
+            ArrayAdapter<String> aa = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
             return rootView;
         }
     }
